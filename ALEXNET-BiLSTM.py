e3fc5528bd8d9362b2024855ee10aaeeebae6844
mahsaabahrami <87757514+mahsaabahrami@users.noreply.github.com>
2021-12-10 10:23:43 +0330

    Update ALEXNET-BiLSTM.py

--- ALEXNET-BiLSTM.py
+++ ALEXNET-BiLSTM.py
@@ -9,7 +9,7 @@ import pickle
 import numpy as np
 import os
 from keras.callbacks import LearningRateScheduler,EarlyStopping
-from keras.layers import Dense,Flatten,MaxPooling2D,Conv2D,Permute,Reshape,GRU,BatchNormalization,Bidirectional
+from keras.layers import Dense,Flatten,MaxPooling2D,Conv2D,Permute,Reshape,LSTM,BatchNormalization,Bidirectional
 from keras.regularizers import l2
 from scipy.interpolate import splev, splrep
 from sklearn.model_selection import train_test_split
