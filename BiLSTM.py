f0648b67326e0cc5b6a137c998fb10d224c5d369
mahsaabahrami <87757514+mahsaabahrami@users.noreply.github.com>
2021-12-10 09:55:18 +0330

    Update BiLSTM.py

--- BiLSTM.py
+++ BiLSTM.py
@@ -9,7 +9,7 @@ import pickle
 import numpy as np
 import os
 from keras.callbacks import LearningRateScheduler,EarlyStopping
-from keras.layers import Dense, Bidirectional,LSTM
+from keras.layers import Dense, Bidirectional,LSTM,Flatten
 from keras.regularizers import l2
 from scipy.interpolate import splev, splrep
 from sklearn.model_selection import train_test_split
